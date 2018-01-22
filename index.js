29fcfcb16944f22fff88f37693d91a276a8fc704
Richard Kim <richeekim@gmail.com>
2018-01-22 23:47:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  //const two = 2
+
 }
 
 var funkyFunction = function() {
-  function() {
+  return function() {
     return "FUNKY!"
   }
 }
